370436640d7f756feee118a465dcf6031287b6f4
Ian <dsouzaian2000@gmail.com>
2024-06-19 00:06:14 +0530

    React removed

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { useMapsLibrary } from "@vis.gl/react-google-maps";
 import { TextField } from "@radix-ui/themes";
 
